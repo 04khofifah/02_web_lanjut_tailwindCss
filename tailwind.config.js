38cb2434da2f9feffee8cef7512f9f9f0d2011f9
04khofifah <2041720119@student.polinema.ac.id>
2022-02-21 15:35:36 +0700

    update tailwin.config untuk menabahkan content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-  content: [],
+  content: [
+      "./resources/**/*.blade.php",
+      "./resource/**/*.js",
+      "./resource/**/*.vue",
+
+  ],
   theme: {
     extend: {},
   },
